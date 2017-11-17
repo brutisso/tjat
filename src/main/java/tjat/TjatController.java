7379f7c34fb7d9988b9497ba2353f4cfd890ce93
brutisso <bengt.rutisson@gmail.com>
2017-11-17 23:11:58 +0100

    Add html template

--- src/main/java/tjat/TjatController.java
+++ src/main/java/tjat/TjatController.java
@@ -1,14 +1,28 @@
 package tjat;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class TjatController {
 
     @RequestMapping("/")
     public String index() {
         return "Tjat, tjat, tjat...";
     }
+
+    @RequestMapping("/tjat")
+    public String greeting(
+    		@RequestParam(value="name", required=false, defaultValue="???") String name,
+    		@RequestParam(value="message", required=false, defaultValue="Nothing") String message,
+    		Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("message", message);
+        System.out.println("msg: " + message);
+        return "tjat";
+    }
+
 
 }
