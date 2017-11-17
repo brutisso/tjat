4f44ebea8045e2a66630123901dd2f0a6aae6f8e
brutisso <bengt.rutisson@gmail.com>
2017-11-18 00:09:15 +0100

    Messages in list

--- src/main/java/tjat/TjatController.java
+++ src/main/java/tjat/TjatController.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TjatController {
+	
+	private List messages = new ArrayList(); 
 
     @RequestMapping("/")
     public String index() {
@@ -18,8 +22,10 @@ public class TjatController {
     		@RequestParam(value="name", required=false, defaultValue="???") String name,
     		@RequestParam(value="message", required=false, defaultValue="Nothing") String message,
     		Model model) {
+    	messages.add(message);
         model.addAttribute("name", name);
         model.addAttribute("message", message);
+        model.addAttribute("messages", messages);
         System.out.println("msg: " + message);
         return "tjat";
     }
